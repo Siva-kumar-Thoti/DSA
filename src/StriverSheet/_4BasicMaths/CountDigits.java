044903f15f792951245f2b2e663f956ed9ec7045
Siva kumar Thoti <thotisiva2000@gmail.com>
2025-11-01 19:06:07 +0530

    Java DSA practice

--- src/StriverSheet/_4BasicMaths/CountDigits.java
+++ src/StriverSheet/_4BasicMaths/CountDigits.java
@@ -1,10 +1,14 @@
 package StriverSheet._4BasicMaths;
 
+import java.util.Scanner;
+
 public class CountDigits {
 
   public static void main(String[] args) {
-    int n=123;
-    System.out.println(count(n));
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+    System.out.println(count(num));
   }
 
   static int count(int n){
